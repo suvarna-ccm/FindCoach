427f9ed1394a9435cfc3823454290b5ec0dce9da
suvarna <suvarna.kumari@v2solutions.com>
2021-09-06 16:57:53 +0530

    added comments

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 import store from './store/index.js';
-
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
@@ -15,6 +14,8 @@ const app = createApp(App);
 
 app.use( router );
 app.use( store );
+
+// Creating custom components
 
 app.component( 'base-card',BaseCard );
 app.component( 'base-button',BaseButton );
